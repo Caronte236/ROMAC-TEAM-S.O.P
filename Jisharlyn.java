fed8880bb1dc4c39de66dd5936d485d9940d53f0
jisharlyn <jisharlynmiguel@gmail.com>
2026-09-20 23:46:17 -0600

    hola

--- Jisharlyn.java
+++ Jisharlyn.java
@@ -1,19 +1,19 @@
 
 /**
- * Write a description of class jisharly here.
+ * Write a description of class Jisharlyn here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class jisharly
+public class Jisharlyn
 {
     // instance variables - replace the example below with your own
     private int x;
 
     /**
-     * Constructor for objects of class jisharly
+     * Constructor for objects of class Jisharlyn
      */
-    public jisharly()
+    public Jisharlyn()
     {
         // initialise instance variables
         x = 0;
